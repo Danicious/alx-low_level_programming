261dbc4ea7a56746bcdfb392a77223c9c32d2255
Danicious <ajayidaniel99@gmail.com>
2022-10-15 01:20:31 -0700

    alphabets and numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints numbers in base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char lc;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (lc = 'a'; la <= 'f'; la++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+return (0);
+}
