841d87969bdbd2f863435452280334b4c0c4b28c
Danicious <ajayidaniel99@gmail.com>
2022-10-18 14:58:46 -0700

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ */
+void print_alphabet(void);
+{
+	char lw;
+
+	for (lw = 'a', lw <= 'z', lw++)
+		_putchar(lw);
+
+	_putchar('\n');
+return (0);
+}
