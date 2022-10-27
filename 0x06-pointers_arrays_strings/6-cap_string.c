441673065fbde88bed3d1cc798d562d4b205c408
Danicious <ajayidaniel99@gmail.com>
2022-10-27 16:18:10 -0700

    capitaling all strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalize words
+ * cap_string - Capitalizes words
  * @str: string
  * Return: pointer to string
  */
@@ -11,23 +11,22 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-
+		while (!(str[i] >= 'a' &&  str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == 'n' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0
+		 str[i - 1] == '\t' ||
+		 str[i - 1] == '\n' ||
+		 str[i - 1] == ',' ||
+		 str[i - 1] == ';' ||
+		 str[i - 1] == '.' ||
+		 str[i - 1] == '!' ||
+		 str[i - 1] == '?' ||
+		 str[i - 1] == 'n' ||
+		 str[i - 1] == '(' ||
+		 str[i - 1] == ')' ||
+		 str[i - 1] == '{' ||
+		 str[i - 1] == '}' ||
+		 i == 0)
 			str[i] -= 32;
 
 		i++;
@@ -35,3 +34,4 @@ char *cap_string(char *str)
 
 	return (str);
 }
+
